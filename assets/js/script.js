50ea6203f90288aa5807d4af95ad5d9681fad810
Gabriel Leme dos Santos <gabriellemesantos@gmail.com>
2023-02-07 00:11:55 -0300

    edit no js - function typewriter

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+//* animação 'span' - título*//
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 110 * i);
+    });
+  }
+
+  const titulo = document.querySelector('span');
+  typeWriter(titulo);
+
+  //* animação 'p' - descrição *//
+
+  function typeWriter2(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 70 * i);
+    });
+  }
+
+  const subtitulo = document.querySelector('p');
+  typeWriter2(subtitulo);
